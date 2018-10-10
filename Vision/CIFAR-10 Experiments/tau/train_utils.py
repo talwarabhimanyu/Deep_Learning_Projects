c007d41bd00ea943b1ce9c9a257b29451373f510
Abhimanyu Talwar <p10abhimanyut@iima.ac.in>
2018-10-09 21:18:09 -0400

    fixed device for gpu

--- Vision/CIFAR-10 Experiments/tau/train_utils.py
+++ Vision/CIFAR-10 Experiments/tau/train_utils.py
@@ -74,7 +74,7 @@ class NeuralNet():
         if ('resnet' in self.pred_fn_class):
             base_model.avgpool = nn.AdaptiveAvgPool2d(1)
             base_model.fc = nn.Linear(base_model.fc.in_features, self.num_classes)
-        base_model = base_model.to(device)
+        base_model = base_model.to(self.device)
         return base_model
 
     def resetModel(self):
