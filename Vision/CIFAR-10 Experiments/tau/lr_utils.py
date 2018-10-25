30b96192e74eeeffd140f25eb81f936daa4e9fd3
Abhimanyu Talwar <p10abhimanyut@iima.ac.in>
2018-10-25 13:21:27 -0400

    fixed to(devide) in makeGIF

--- Vision/CIFAR-10 Experiments/tau/lr_utils.py
+++ Vision/CIFAR-10 Experiments/tau/lr_utils.py
@@ -164,7 +164,7 @@ class WeightWatcher(Callback):
         self.epoch_count = 0
     
     def saveWeights(self):
-        layer_matrix = self.optimizer.param_groups[0]['params'][0].detach().numpy()
+        layer_matrix = self.optimizer.param_groups[0]['params'][0].detach().to(self.device).numpy()
         num_filters = layer_matrix.shape[0]
         layer_matrix = layer_matrix.transpose(0,2,3,1)
         num_cols = 8
